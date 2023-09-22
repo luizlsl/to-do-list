80343361af0e15d37d04aa4bacad46967918083b
luizlsl <luiz.lsldf@gmail.com>
2023-09-22 20:55:05 -0300

    correções

--- index.js
+++ index.js
@@ -4,23 +4,25 @@ const app = express();
 //Express ultilizando EJS como View engine
 app.set("view engine", "ejs");
 
+app.use(express.static('public'));
+
 app.get("/", (req, res) => {
   var nome = "Luiz Silva"; //Criando a variável nome
   var lang = "Python, JS, c++"; //Criando a variável lang
   var fram = "Django, React, ExpressJS e PyQT"; //Cria variavel fram
   var area = "Pricipal Front-End e o necessario do Back-End"; //Cria variavel area
-  var lings = [
-    {nome:"HTML", n:"65%"},
-    {nome:"CSS", n:"45%"},
-    {nome:"python", n:"39%"},
-    {nome:"javaScript", n:"10%"}
-  ]
+  // var lings = [
+  //   {nome:"HTML", n:"65%"},
+  //   {nome:"CSS", n:"45%"},
+  //   {nome:"python", n:"39%"},
+  //   {nome:"javaScript", n:"10%"}
+  // ]
   res.render("index",{
     nome: nome,
     lang: lang,
     fram: fram,
     area: area,
-    lings: lings
+    // lings: lings
   }); // Renderiza o index.js na tela
   //Ultilizando o "Render" ele olha automaticamente o arquivo que está na pasta "views"
 });
